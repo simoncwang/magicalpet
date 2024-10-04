ce282904c77586289628bc355d19ea34507e9d32
Angelyn Pham <angpham@terpmail.umd.edu>
2024-10-04 14:18:40 -0400

    20 seconds

--- live_emotion_detection.py
+++ live_emotion_detection.py
@@ -8,13 +8,18 @@ from util import *
 # MTCNN detects faces and facial landmarks on images
 emotion_detector = FER(mtcnn=True)   # NOTE: setting this to false uses Haar
 
+# Initialize last emotion data to hold the emotion and last update time
+last_emotion_data = {"emotion": "neutral", "last_update": time.time()}
+
 # continuously capturing webcam feed until user presses esc
 cam = cv2.VideoCapture(1)
+
 while True:
     result, img = cam.read()
     cv2.imshow('my webcam', displayAnalysis(img,emotion_detector))
     if cv2.waitKey(1) == 27: 
         break  # esc to quit
+        
 cam.release()
 cv2.destroyAllWindows()
 
--- util.py
+++ util.py
@@ -28,42 +28,41 @@ def captureImage():
     else:
         print("image read error")
 
-def displayAnalysis(img, emotion_detector):
+def displayAnalysis(img, emotion_detector, last_emotion_data):
     # analyzing the emotions in the image including bounding box
     analysis = emotion_detector.detect_emotions(img)
 
-    # getting the dominant emotion and its score
-    dominant_emotion, emotion_score = emotion_detector.top_emotion(img)
+    if analysis:
+        # getting the dominant emotion and its score
+        dominant_emotion, emotion_score = emotion_detector.top_emotion(img)
 
-    # parameters for the rectangles we want to draw
-    color = (0,255,0)   # in b,g,r format
-    thickness = 2
+        # Check if 20 seconds have passed since the last emotion change
+        if time.time() - last_emotion_data["last_update"] >= 20:
+            # Update the current emotion after 20 seconds
+            last_emotion_data["emotion"] = dominant_emotion
+            last_emotion_data["last_update"] = time.time()
+        else:
+            # Keep the last emotion if 20 seconds haven't passed
+            dominant_emotion = last_emotion_data["emotion"]
 
-    # drawing a rectangle around all the detected faces
-    for face in analysis:
-        # getting the bounding box coordinates
-        box = face["box"]
+        # parameters for the rectangles we want to draw
+        color = (0, 255, 0)   # in b,g,r format
+        thickness = 2
 
-        # getting x,y,width,height of the box
-        x = box[0]
-        y = box[1]
-        w = box[2]
-        h = box[3]
+        # drawing a rectangle around all the detected faces
+        for face in analysis:
+            # getting the bounding box coordinates
+            box = face["box"]
+            x, y, w, h = box
 
-        # getting start point x,y coordinates
-        start = (x, y)
-        # getting end point x,y coordinates
-        end = (x+w, y+h)
+            # drawing the rectangle
+            start = (x, y)
+            end = (x + w, y + h)
+            img = cv2.rectangle(img, start, end, color, thickness)
 
-        # drawing the rectangle
-        img = cv2.rectangle(img, start, end, color, thickness)
-
-        # getting the emotions
-        emotions = analysis[0]["emotions"]
-
-        # writing the emotion next to the rectangle
-        # cv2.putText(img, str(emotions), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-        cv2.putText(img, dominant_emotion + ": "  + str(emotion_score), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            # writing the dominant emotion next to the rectangle
+            cv2.putText(img, dominant_emotion + ": " + str(emotion_score), (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
-    # return the image
+    # return the image with analysis
     return img
