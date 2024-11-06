eb46b200f6dc592c782afb495c2e4898a369a77e
Grace Ko <gracek7689@gmail.com>
2024-11-06 15:10:53 -0500

    Added functionality for turtle to move backwards and forwards when happy and sad

--- MagicalPet/src/main.cpp
+++ MagicalPet/src/main.cpp
@@ -8,16 +8,34 @@ void angle(int a);
 const int redPin = 32; 
 const int greenPin = 33; 
 
+// Pins for wheels
+// Original Pins
+const int motorPin1 = 18;  // IN1 (GPIO 18)
+const int motorPin2 = 19;  // IN2 (GPIO 19)
+
+// Secondary Pins
+const int motorPin3 = 16;  // IN1 (GPIO 18)
+const int motorPin4 = 17;  // IN2 (GPIO 19)
+
 void right_open();
 void left_open();
 void left_close();
 void right_close();
+void moveForward();
+void moveBackward();
+void stopMotor();
 
 void setup() {
   Serial.begin(9600);
   pinMode(servoPin, OUTPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
+
+  // Set the motor control pins as outputs
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
 }
 
 void loop() {
@@ -25,16 +43,30 @@ void loop() {
     data = Serial.read();
 
     if (data == '0') {
+      // Reacting for happy emotion
+      moveForward();
+      delay(5000);
+      stopMotor();
       right_open();
       digitalWrite(greenPin, HIGH); // set the LED on
       delay(15000);
       right_close();
+      moveBackward();
+      delay(5000);
+      stopMotor();
       digitalWrite(greenPin, LOW); // set the LED off
     } else if (data == '1') {
+      // Reacting for negative emotion
+      moveBackward();
+      delay(5000);
+      stopMotor();
       left_open();
       digitalWrite(redPin, HIGH); // set the LED on
       delay(15000);
       left_close();
+      moveForward();
+      delay(5000);
+      stopMotor();
       digitalWrite(redPin, LOW); // set the LED off
     } else {
       Serial.println("Invalid input");
@@ -73,6 +105,31 @@ void angle(int a) {
   digitalWrite(servoPin, LOW);
   delayMicroseconds(20000-pulseWidth);
 
+}
+
+// Function to move motor forward
+void moveForward() {
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  Serial.println("Moving forward");
+}
+
+void moveBackward() {
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  Serial.println("Moving backward");
+}
+
+void stopMotor() {
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  Serial.println("Stopped");
 }
 
 // #include <Arduino.h>
